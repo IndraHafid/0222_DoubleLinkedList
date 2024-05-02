dc614a278df307d9178f12c871a1ee5ffc68bc0e
IndraHfid <indrahafidsaputra@gmail.com>
2024-05-02 15:31:12 +0700

    Add project files.

---
+++ 0222_LinkedList/0222_LinkedList.cpp
@@ -0,0 +1,14 @@
+// 0222_LinkedList.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+   
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
